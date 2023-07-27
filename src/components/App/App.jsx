ff2312b44edfc7d98127d5092a74abbda1a9c97a
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-07-27 20:36:22 +0200

    create

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,82 +1,16 @@
 import React, { Component } from 'react';
-import shortid from 'shortid';
-import css from './App.module.css';
-import ContactForm from 'components/ContactForm/ContactForm';
-import Filter from 'components/Filter/Filter';
-import ContactList from 'components/ContactList/ContactList';
+// import shortid from 'shortid';
+// import css from './App.module.css';
 
 class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+  state = {};
 
-  componentDidMount(nextProps, prevState) {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  componentDidMount(nextProps, prevState) {}
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  onFilterChange = event => {
-    this.setState({ filter: event.currentTarget.value });
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  addContact = (name, number) => {
-    const { contacts } = this.state;
-    if (contacts.some(contact => contact.name === name)) {
-      return alert(`${name} is already in contacts`);
-    }
-
-    const newContact = {
-      id: shortid.generate(),
-      name: name.trim(),
-      number: number,
-    };
-
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
-  };
-
-  filterAddContacts = () => {
-    const { contacts, filter } = this.state;
-
-    const normalizedFilter = filter.toLowerCase();
-    const visibleContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-
-    return visibleContacts;
-  };
+  componentDidUpdate(prevProps, prevState, snapshot) {}
 
   render() {
-    return (
-      <div className={css.wrapper}>
-        <h1 className={css.title}>Phonebook</h1>
-
-        <ContactForm contacts={this.contacts} addContact={this.addContact} />
-        <h2 className={css.title}>Contacts</h2>
-        <Filter onFilterChange={this.onFilterChange} />
-        <ContactList
-          contacts={this.filterAddContacts()}
-          deleteContacts={this.deleteContact}
-        />
-      </div>
-    );
+    return;
   }
 }
 
